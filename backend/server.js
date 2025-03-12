6a79c42be6a427168ea573fa4a2117ea4aa867c0
Maxwell011 <chukwuemekaorji678@gmail.com>
2025-03-12 20:43:13 +0100

    Add CORS middleware to server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import newsRoutes from "./routes/news.route.js";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api", newsRoutes);
