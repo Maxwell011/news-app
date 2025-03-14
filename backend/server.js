4deec55e003867251b5f01530d06e912dd710dc5
Maxwell011 <chukwuemekaorji678@gmail.com>
2025-03-14 22:25:24 +0100

    Comment out server initialization code for future reference

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { connectDB } from "./config/db.js";
-import newsRoutes from "./routes/news.route.js";
-import adminRoutes from "./routes/admin.route.js";
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import { connectDB } from "./config/db.js";
+// import newsRoutes from "./routes/news.route.js";
+// import adminRoutes from "./routes/admin.route.js";
 
-dotenv.config();
-const app = express();
-app.use(cors());
-const PORT = process.env.PORT || 3000;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// const PORT = process.env.PORT || 3000;
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", newsRoutes);
-app.use("/api/auth", adminRoutes);
+// app.use("/api", newsRoutes);
+// app.use("/api/auth", adminRoutes);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server started at http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   connectDB();
+//   console.log(`Server started at http://localhost:${PORT}`);
+// });
