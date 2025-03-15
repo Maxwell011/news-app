16c0a501fc2c2bd4acf5e4cb5c2ba0a0af4ec73a
Maxwell011 <chukwuemekaorji678@gmail.com>
2025-03-15 14:36:47 +0100

    Implement routing for news and tagging features with React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,32 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import NewsList from "./components/NewsList";
+import NewsDetail from "./components/NewsDetail";
+import TagNews from "./components/TagNews";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
-      <div className="pt-32">
-        <h1 className="text-black text-center text-6xl mt-20 uppercase">
-          {" "}
-          Kiras Newsroom
-        </h1>
-      </div>
-      <NewsList />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="pt-32">
+                <h1 className="text-black text-center text-6xl mt-20 uppercase">
+                  Kiras Newsroom
+                </h1>
+              </div>
+              <NewsList />
+            </>
+          }
+        />
+        <Route path="/news/:id" element={<NewsDetail />} />
+        <Route path="/tag/:tag" element={<TagNews />} />
+      </Routes>
     </>
   );
 }
